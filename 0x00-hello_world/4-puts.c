b0c2dac49069981fa91b0e156b1392151172da55
Tebogo Vanessa Ratau <trtratau@gmail.com>
2023-04-23 08:49:02 -0700

    Task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* Function print line: Programming is like building a multilingual puzzle
+ * Using puts
+ * return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
