da27a95f3f972c5b87f14b910f5bf2a959d3905a
Tebogo Vanessa Ratau <trtratau@gmail.com>
2023-04-23 09:20:09 -0700

    Task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints new line using printf
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
